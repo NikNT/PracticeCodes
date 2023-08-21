9e3c2e0dfb2b174d993ca5dbd2bdcb5cf55bf57d
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-08-21 08:48:33 -0400

    10. TypeScript - Interfaces

--- 10. TypeScript/app.ts
+++ 10. TypeScript/app.ts
@@ -26,23 +26,98 @@
 
 // Type Alias
 
-type User = {
+// type User = {
+//   name: string;
+//   age: number;
+//   address?: string;
+// };
+
+// const user: User = {
+//   name: "Nik",
+//   age: 25,
+// };
+
+// function login(userData: User): User {
+//   return userData;
+// }
+
+// console.log(login({ name: "J Doe", age: 20 }));
+
+// type ID = number;
+
+// const userID: ID = 23;
+
+//Interfaces
+
+// interface Transaction {
+//   payerAccountNumber: number;
+//   payeeAccountNumber: number;
+// }
+
+// interface BankAccount {
+//   accountNumber: number;
+//   accountHolder: string;
+//   balance: number;
+//   isActive: boolean;
+//   transaction: Transaction[];
+// }
+
+// const transaction1: Transaction = {
+//   payeeAccountNumber: 1234,
+//   payerAccountNumber: 4567,
+// };
+
+// const transaction2: Transaction = {
+//   payeeAccountNumber: 1234,
+//   payerAccountNumber: 5678,
+// };
+
+// const bankAccount: BankAccount = {
+//   accountNumber: 123,
+//   accountHolder: "Nik",
+//   balance: 5000,
+//   isActive: true,
+//   transaction: [transaction1, transaction2 ],
+// };
+
+// Extend Interfaces
+
+interface Book {
   name: string;
-  age: number;
-  address?: string;
-};
-
-const user: User = {
-  name: "Nik",
-  age: 25,
-};
-
-function login(userData: User): User {
-  return userData;
+  price: number;
 }
 
-console.log(login({ name: "J Doe", age: 20 }));
+interface EBook {
+  name: string;
+  price: number;
+  fileSize: number;
+  format: string;
+}
 
-type ID = number;
+interface AudioBook {
+  name: string;
+  price: number;
+  fileSize: number;
+  format: string;
+  duration: number;
+}
 
-const userID: ID = 23;
+const book: Book = {
+  name: "Shoe Dog",
+  price: 25,
+};
+
+const ebook: EBook = {
+  name: "Atomic Habits",
+  price: 25,
+  fileSize: 300,
+  format: "pdf",
+};
+
+const audioBook: AudioBook = {
+  name: "Atomic Habits",
+  price: 25,
+  fileSize: 300,
+  format: "pdf",
+  duration: 5,
+};
