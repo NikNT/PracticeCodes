54ad458dbf81cb8c76484b7743e3e848d0abf566
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-09-20 21:31:42 -0400

    13. WhyNote - MongoDB Setup

--- 13. WhyNote/backend/server.js
+++ 13. WhyNote/backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const notesRoutes = require("./routes/notesRoutes");
 require("dotenv").config();
 const PORT = process.env.PORT;
+const mongoURI = process.env.MONGO_URI;
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -11,6 +13,15 @@ app.use((req, res, next) => {
 });
 app.use("/api/notes", notesRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Listening on Port ${PORT}`);
-});
+try {
+  mongoose
+    .connect(mongoURI)
+    .then(() => {
+      app.listen(PORT, () => {
+        console.log(`MongoDB Connected | Listening on Port ${PORT}`);
+      });
+    })
+    .catch((err) => console.error(err));
+} catch (err) {
+  console.error(err, "Error : Server/Mongo");
+}
