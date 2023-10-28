bd6e6af983bb221f2c21f072d1df998c54e25a6d
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-27 21:35:54 -0400

    17. MernAuth - Routes Add

--- 17. MernAuth/backend/controllers/userController.js
+++ 17. MernAuth/backend/controllers/userController.js
@@ -1,5 +1,11 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Auth user/set token
 // route POST /api/users/auth
-const authUser = (req, res) => {
+// @access Public
+
+const authUser = asyncHandler((req, res) => {
   res.status(200).json({ message: "Auth User" });
-};
+});
+
+export { authUser };
