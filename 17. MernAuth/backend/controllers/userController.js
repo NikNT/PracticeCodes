6c7fde0bea6f300e840a91d0c771228ca2ecaa46
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-10-28 22:06:49 -0400

    17. MernAuth - Routes Setup

--- 17. MernAuth/backend/controllers/userController.js
+++ 17. MernAuth/backend/controllers/userController.js
@@ -8,4 +8,42 @@ const authUser = asyncHandler((req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
-export { authUser };
+// @desc Regsiter a new user
+// route POST /api/users/
+// @access Public
+
+const registerUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access Public
+
+const logoutUser = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc GET user profile
+// route GET /api/users/profile
+// @access Private
+
+const getUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc UPDATE user profile
+// route PUT /api/users/profile
+// @access Private
+
+const updateUserProfile = asyncHandler((req, res) => {
+  res.status(200).json({ message: "Update User Profile" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
