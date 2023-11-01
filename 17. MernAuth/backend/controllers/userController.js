f848545afa2f6c93b878f1d55ecd6bf18952a1e7
Nikhil Tanwar <nikhiltanwarnt@gmail.com>
2023-11-01 18:15:00 -0400

    17. MernAuth - DB and JWT Setup

--- 17. MernAuth/backend/controllers/userController.js
+++ 17. MernAuth/backend/controllers/userController.js
@@ -1,10 +1,11 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 
-const authUser = asyncHandler((req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Auth User" });
 });
 
@@ -12,15 +13,36 @@ const authUser = asyncHandler((req, res) => {
 // route POST /api/users/
 // @access Public
 
-const registerUser = asyncHandler((req, res) => {
-  res.status(200).json({ message: "Register User" });
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout user
 // route POST /api/users/logout
 // @access Public
 
-const logoutUser = asyncHandler((req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Logout User" });
 });
 
@@ -28,7 +50,7 @@ const logoutUser = asyncHandler((req, res) => {
 // route GET /api/users/profile
 // @access Private
 
-const getUserProfile = asyncHandler((req, res) => {
+const getUserProfile = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "User Profile" });
 });
 
@@ -36,7 +58,7 @@ const getUserProfile = asyncHandler((req, res) => {
 // route PUT /api/users/profile
 // @access Private
 
-const updateUserProfile = asyncHandler((req, res) => {
+const updateUserProfile = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Update User Profile" });
 });
 
--- 17. MernAuth/backend/server.js
+++ 17. MernAuth/backend/server.js
@@ -7,6 +7,8 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 connectDB();
 app.use("/api/users", userRoutes);
 
